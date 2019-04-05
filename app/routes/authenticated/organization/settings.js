8f5a3f63d66a5d5e05b0ca6ce369b0f18f043656
Praseetha-KR <praseetha04@gmail.com>
2019-04-05 18:32:56 +0530

    Added mandate mfa setting for organization

--- app/routes/authenticated/organization/settings.js
+++ app/routes/authenticated/organization/settings.js
@@ -1,7 +1,14 @@
 import Route from '@ember/routing/route';
+import config from 'irene/config/environment';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  model: function(){
-    return this.get("store").query('organization', {id: null});
+  organization: service(),
+
+  title: `Settings${config.platform}`,
+
+  async model(){
+    const orgId = this.get('organization').selected.id;
+    return await this.get('store').find('organization', orgId);
   }
 });
